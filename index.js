e6d896ec7e16186dd9408b308167a5287b03442e
arzuashik <arzu2050@gmail.com>
2022-10-18 02:54:40 +0600

    home page added and port issue fix

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const usersRoutes = require("./routes/v1/users.route.js");
 
 app.use(cors());
 app.use(express.json());
+app.set("view engine", "ejs");
 
 // app.use(viewCount);
 
@@ -16,7 +18,7 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 app.get("/", (req, res) => {
-  res.send("hlsdf");
+  res.render("home.ejs");
 })
 app.use("/api/v1/users", usersRoutes);
 
